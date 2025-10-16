2b9b51659c91259c88f632d893386fd023371211
Alessandra <alessandrag408@gmail.com>
2025-10-17 00:13:07 +0200

    sistemato input e aggiunto le funzioni per gestire l'input e l'invio dek form

--- form-Alessandro/react/src/pages/Home.jsx
+++ form-Alessandro/react/src/pages/Home.jsx
@@ -7,13 +7,47 @@ const Home = () => {
         email: '',
         phone: ''
     })
+
+    const handleInput = (event) => {
+        const{target:{name, value}} = event
+        setForm(f => ({...f, [name]:value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="firstName">FirstName</label>
                     <div>
-                        <input type="text" id='firstName' name="firstName" />
+                        <input onInput={handleInput} value={form.firstName} type="text" id='firstName' name="firstName" />
+                    </div>
+                </div>
+            </form>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="lastName">LastName</label>
+                    <div>
+                        <input onInput={handleInput} value={form.lastName} type="text" id='lastName' name="lastName" />
+                    </div>
+                </div>
+            </form>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <div>
+                        <input onInput={handleInput} value={form.email} type="email" id='email' name="email" />
+                    </div>
+                </div>
+            </form>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="phone">Phone</label>
+                    <div>
+                        <input onInput={handleInput} value={form.phone} type="tel" id='phone' name="phone" />
                     </div>
                 </div>
             </form>
