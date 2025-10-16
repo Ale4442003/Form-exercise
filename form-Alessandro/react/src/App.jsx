b7ebec06e4c458bd5d3e3a8921437090b5c034d9
Alessandra <alessandrag408@gmail.com>
2025-10-16 23:53:59 +0200

    creato cartelle store e inserito rotte

--- form-Alessandro/react/src/App.jsx
+++ form-Alessandro/react/src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import Confirm from './pages/Confirm'
+import Success from './pages/Success'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/confirm" element={<Confirm />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </>
   )
 }
 
--- form-Alessandro/react/src/main.jsx
+++ form-Alessandro/react/src/main.jsx
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Provider as ReduxProvider } from 'react-redux'
+import {  BrowserRouter } from 'react-router-dom'
+import store from './store/store.js'
 
 import './index.css'
 
-
 createRoot(document.getElementById('root')).render(
-    <App />
+  <ReduxProvider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ReduxProvider>
 )
